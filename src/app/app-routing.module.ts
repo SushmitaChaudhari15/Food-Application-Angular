3bb7b38059a267fc3204f94a937fc36fcc376c52
Sushmita <sushmitachaudhari15@gmail.com>
2022-04-19 19:05:40 +0530

    Seeting up All components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path:'' ,redirectTo:'home',pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'menu', component:MenuComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'**',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
