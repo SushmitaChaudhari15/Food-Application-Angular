e071e027465b072454d7def54c1298d55e1f0525
Sushmita <sushmitachaudhari15@gmail.com>
2022-04-21 18:22:14 +0530

    done with home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodDetailsService } from 'src/app/services/food-details.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  foodDetails:any;
+  constructor(private foodService:FoodDetailsService) { }
 
   ngOnInit(): void {
+    this.foodDetails= this.foodService.foodDetails;
   }
+
+
 
 }
